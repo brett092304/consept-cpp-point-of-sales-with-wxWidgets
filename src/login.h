46081574244d5257f64fd96684f4534f50c71699
brett092304 <84330357+brett092304@users.noreply.github.com>
2022-03-15 13:36:42 -0500

    Add files via upload

---
+++ src/login.h
@@ -0,0 +1,17 @@
+#include "PCH.h"
+
+class loginDialog : public wxDialog
+{
+    public:
+        loginDialog(const wxString &title, bool isManagerLogin);
+        void OnClose(wxCommandEvent &event);
+        void OnLogIn(wxCommandEvent &event);
+        void logIn();
+        void enterKeyPad(wxCommandEvent &event);
+        std::string hashFunc(std::string password);
+    private:
+        wxTextCtrl *loginUserBox;
+        wxTextCtrl *loginPassBox;
+        bool enteringUser;
+        bool isTempManagerLogin = false;
+};
